28ac0dee6f39f6b503bc79a6075893e816a2990a
ckwogus37 <ckwogus37@gmail.com>
2024-06-07 19:09:01 +0900

    Sprint 3rd Project - 도서 쇼핑몰 프론트엔드 코드와 연결 cors 라이브러리 설치 및 사용

--- app.js
+++ app.js
@@ -13,10 +13,20 @@ const cartsRouter = require('./routes/carts');
 const likesRouter = require('./routes/likes');
 const ordersRouter = require('./routes/orders');
 
+const cors = require("cors");
+const corsOptions = {
+  origin: process.env.CLIENT_ORIGIN || "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 204,
+  credentials : true
+};
+
+app.use(cors(corsOptions));
+
 app.use('/users', userRouter);
 app.use('/books',booksRouter);
 app.use('/category',categoryRouter);
 app.use('/likes',likesRouter);
 app.use('/carts', cartsRouter);
 app.use('/orders',ordersRouter);
-
